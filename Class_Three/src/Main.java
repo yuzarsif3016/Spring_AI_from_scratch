92f9f4a6347169a5e693dd9eeb144c47159ec268
yuzarsif3016 <yusufpathan3016@gmail.com>
2025-08-03 15:35:18 +0530

    first commit

---
+++ Class_Three/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 10 , b = 20;
+
+        if(a > b){
+            System.out.println("A is bigger");
+        }
+        else{
+            System.out.println("B is bigger");
+        }
+    }
+
+}
