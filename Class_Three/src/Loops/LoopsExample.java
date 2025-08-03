e5a4014e37595cc4a9086400670c5d637283733b
yuzarsif3016 <yusufpathan3016@gmail.com>
2025-08-03 15:54:59 +0530

    loops in java

---
+++ Class_Three/src/Loops/LoopsExample.java
@@ -0,0 +1,22 @@
+package Loops;
+
+public class LoopsExample {
+    public static void main(String[] args) {
+        System.out.println("focus is the key");
+
+//        for (int i = 1; i <= 5; i++) {
+//            System.out.println("value of i is : " + i);
+//        }
+
+        int i=1;
+
+//        while(i < 6){
+//            System.out.println("Value of i  is " + i);
+//            i++;
+//        }
+
+        do{
+            System.out.println("Value of i : " + i);
+        }while(i > 1);
+    }
+}
