bfaca73ef61a970cc1c3a4cd094ff42e8e05930f
yuzarsif3016 <yusufpathan3016@gmail.com>
2025-08-06 17:39:15 +0530

    class four commit

---
+++ ClassFour/src/Operators/Ternary.java
@@ -0,0 +1,13 @@
+package Operators;
+
+public class Ternary {
+    public static void main(String[] args) {
+
+        int a = 20 , b = 40;
+
+        int res = (b > a) ? (a+b) : (a-b);
+
+        System.out.println(res);
+    }
+
+}
